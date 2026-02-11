bc9e58cc226230120424db34a2ddc3792cc0f63d
faida.a.espiritu@gmail.com <fespiritu@my.bcit.ca>
2026-02-10 17:40:25 -0800

    Fixed book class display()

--- src/code/ca/bcit/comp2522/lab4/Book.java
+++ src/code/ca/bcit/comp2522/lab4/Book.java
@@ -148,7 +148,7 @@ public class Book implements Comparable<Book>,
     public void display()
     {
         System.out.println("\nTitle: " + getTitle() +
-                ", Year: " + getYearPublished());
+                "\nYear: " + getYearPublished());
         System.out.print("Author: ");
         getAuthor().display();
     }
